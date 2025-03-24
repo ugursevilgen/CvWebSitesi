8dc82411fbda8c2b9fb31c85770bcd1f610e5c12
Uğur Sevilgen <ugursevilgen@gmail.com>
2025-03-24 16:33:20 +0300

    Add project files.

---
+++ Controllers/DefaultController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCv.Models.Entity;
+
+namespace MvcCv.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+
+        DbCvEntities db = new DbCvEntities();
+
+        public ActionResult Index()
+        {
+            var degerler = db.TblHakkinda.ToList();
+
+            return View(degerler);
+        }
+
+        public PartialViewResult Deneyim()
+
+        {
+            var deneyimler = db.TblDeneyimlerim.ToList();
+            return PartialView(deneyimler);
+
+        }
+        public PartialViewResult Egitimlerim()
+
+        {
+            var egitimler = db.TblEgitimlerim.ToList();
+            return PartialView(egitimler);
+
+        }
+
+        public PartialViewResult Yeteneklerim()
+
+        {
+            var yetenekler = db.TblYeteneklerim.ToList();
+            return PartialView(yetenekler);
+
+        }
+
+        public PartialViewResult Hobilerim()
+
+        {
+            var hobiler = db.TblHobilerim.ToList();
+            return PartialView(hobiler);
+
+        }
+
+        public PartialViewResult Sertifikalar()
+
+        {
+            var sertifikalar = db.TblSertifikalarim.ToList();
+            return PartialView(sertifikalar);
+
+        }
+
+
+    }
+}
