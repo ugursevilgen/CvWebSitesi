303ad0df92c89d9c80b067f34aa83f151c1ddc2a
Uğur Sevilgen <ugursevilgen@gmail.com>
2025-04-14 19:53:12 +0300

    Admin/Deneyim tamamlandı.

--- Controllers/DeneyimController.cs
+++ Controllers/DeneyimController.cs
@@ -48,5 +48,18 @@ namespace MvcCv.Controllers
             TblDeneyimlerim t = repo.Find(x => x.ID == id);
             return View(t);
         }
+
+        [HttpPost]
+        public ActionResult DeneyimGetir(TblDeneyimlerim p)
+        {
+            TblDeneyimlerim t = repo.Find(x => x.ID == p.ID);
+            t.Baslik = p.Baslik;
+            t.AltBaslik = p.AltBaslik;
+            t.Aciklama = p.Aciklama;
+            t.Tarih = p.Tarih;
+            repo.TUpdate(t);
+
+            return RedirectToAction("Index");
+        }
     }
 }
