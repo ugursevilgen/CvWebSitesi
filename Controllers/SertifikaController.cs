6d8ae5ba406a3ec21f2c0e8526ce0a3ecfe044c2
Uğur Sevilgen <ugursevilgen@gmail.com>
2025-04-25 14:53:35 +0300

    Sertifika, İletişim bitti. Hobilerden devam.

---
+++ Controllers/SertifikaController.cs
@@ -0,0 +1,64 @@
+﻿using MvcCv.Models.Entity;
+using MvcCv.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcCv.Controllers
+{
+    public class SertifikaController : Controller
+    {
+        GenericRepository<TblSertifikalarim> repo = new GenericRepository<TblSertifikalarim>();
+
+        public ActionResult Index()
+        {
+            var sertifika = repo.List();
+            return View(sertifika);
+        }
+
+        [HttpGet]
+        public ActionResult SertifikaGetir(int id)
+        {
+            var sertifika = repo.Find(x => x.ID == id);
+            return View(sertifika);
+
+        }
+
+        [HttpPost]
+        public ActionResult SertifikaGetir(TblSertifikalarim t)
+        {
+
+            var sertifika = repo.Find(x => x.ID == t.ID);
+            sertifika.Aciklama = t.Aciklama;
+            sertifika.Tarih = t.Tarih;
+            repo.TUpdate(sertifika);
+            return RedirectToAction("Index");
+
+        }
+
+        public ActionResult SertifikaSil(int id)
+        {
+            TblSertifikalarim t = repo.Find(x => x.ID == id);
+            repo.TDelete(t);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult YeniSertifika()
+        {
+            return View();
+
+        }
+
+        [HttpPost]
+        public ActionResult YeniSertifika(TblSertifikalarim p)
+        {
+            repo.TAdd(p);
+            return RedirectToAction("Index");
+
+        }
+
+    }
+}
---
+++ Controllers/iletisimController.cs
@@ -0,0 +1,22 @@
+﻿using MvcCv.Models.Entity;
+using MvcCv.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcCv.Controllers
+{
+    public class iletisimController : Controller
+    {
+        // GET: iletisim
+        GenericRepository<TblIetisim> repo = new GenericRepository<TblIetisim>();
+
+        public ActionResult Index()
+        {
+            var mesajlar = repo.List();
+            return View(mesajlar);
+        }
+    }
+}
